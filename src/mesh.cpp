9434fd681e766fbecaf7d97c75cea2fd7793a93e
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2017-11-06 11:03:19 +0000

    Fix a type conversion warning

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -23,7 +23,7 @@ void Mesh::draw(Shader shader)
 
 	// draw the mesh
 	glBindVertexArray(VAO);
-	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+	glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(indices.size()), GL_UNSIGNED_INT, 0);
 
 	// reset to defaults
 	glBindVertexArray(0);
